d1660af9a9c0eb8979b5ce4c476a79a346ba1438
einstweilen <einstweilen@users.noreply.github.com>
2026-01-09 20:26:19 +0100

    Fix 32-bit counter overrun in traffic monitoring
    Added handling for 32-bit counter overrun in traffic calculations.

--- vx231v-snmp-demo.sh
+++ vx231v-snmp-demo.sh
@@ -3,6 +3,11 @@
 IP="192.168.1.1"   # router ip
 COMMUNITY="public" # SNMP community string
 INTERVAL=5         # traffic data sample interval in seconds
+MAX_32=4294967296  # 32 bit 
+IF_INDEX=52        # nas9
+
+OID_IN=".1.3.6.1.2.1.2.2.1.10.$IF_INDEX"   # ifInOctets
+OID_OUT=".1.3.6.1.2.1.2.2.1.16.$IF_INDEX"  # ifOutOctets
 
 echo
 echo "TP-Link VX231v SNMP Demo"
@@ -39,16 +44,14 @@ GUEST_IP=$(snmpwalk -v2c -c "$COMMUNITY" "$IP" .1.3.6.1.2.1.4.20.1.2 | grep ": 3
 GUEST_MASK=$(snmpget -v2c -c "$COMMUNITY" "$IP" .1.3.6.1.2.1.4.20.1.3."$GUEST_IP" | awk '{print $NF}')
 
 # --- Network Ranges ---
-echo "--- IP Networks ---"
-echo "Main Network: $LAN_IP / $LAN_MASK"
+echo "--- Networks ---"
+echo "Main Network : $LAN_IP / $LAN_MASK"
 echo "Guest Network: $GUEST_IP / $GUEST_MASK"
 
 # --- Internet Traffic ---
 # Retrieve Counter32 values for ingress and egress traffic
 get_traffic() {
-     IN=$(snmpget -v2c -c "$COMMUNITY" "$IP" .1.3.6.1.2.1.2.2.1.10.52 | awk '{print $NF}')
-    OUT=$(snmpget -v2c -c "$COMMUNITY" "$IP" .1.3.6.1.2.1.2.2.1.16.52 | awk '{print $NF}')
-    echo "$IN $OUT"
+  snmpget -v2c -c "$COMMUNITY" -Oqv "$IP" "$OID_IN" "$OID_OUT" | tr '\n' ' '
 }
 
 echo -e "\n--- Live Internet Traffic  ---"
@@ -57,11 +60,17 @@ SAMPLE1=($(get_traffic))
 sleep "$INTERVAL"
 SAMPLE2=($(get_traffic))
 
-# Calculate bitrate (Bytes to Bits * 8 / interval)
-# Note: Counter32 wrap-around is not handled in this version
-DOWN_BPS=$(( (SAMPLE2[0] - SAMPLE1[0]) * 8 / INTERVAL ))
-UP_BPS=$(( (SAMPLE2[1] - SAMPLE1[1]) * 8 / INTERVAL ))
-echo "Download: $((DOWN_BPS / 1000)) kbit/s | Upload: $((UP_BPS / 1000)) kbit/s"
+DIFF_IN=$(( SAMPLE2[0] - SAMPLE1[0] ))
+[ $DIFF_IN -lt 0 ] && DIFF_IN=$(( DIFF_IN + MAX_32 )) # correcion for 32 bit overrun
+
+DIFF_OUT=$(( SAMPLE2[1] - SAMPLE1[1] ))
+[ $DIFF_OUT -lt 0 ] && DIFF_OUT=$(( DIFF_OUT + MAX_32 )) # correcion for 32 bit overrun
+
+DOWN_KBIT=$(( DIFF_IN * 8 / INTERVAL / 1000 ))
+UP_KBIT=$(( DIFF_OUT * 8 / INTERVAL / 1000 ))
+
+echo "Download: $DOWN_KBIT kbit/s | Upload: $UP_KBIT kbit/s"
+
 
 # --- SECTION 4: Connected Clients (ARP) ---
 # Dynamic walk of ipNetToMediaTable to map IP to MAC
